e23977d0ede9016aac8480bb9de8a35e7e0c5a9a
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-06 11:14:55 -0800

    Added coolscript

---
+++ coolscript.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+def print_pokemon_theme():
+    print("""I wanna be the very best
+Like no one ever was 
+To catch them is my real test 
+To train them is my cause
+
+I will travel across the land
+Searching far and wide
+Each Pokemon to understand
+The power thats inside
+
+Pokemon, gotta catch em all
+Its you and me
+I know its my destiny
+Pokemon
+Oh, youre my best friend
+In a world we must defend
+Pokemon, gotta catch em all
+A heart so true
+Our courage will pull us through
+
+You teach me and Ill teach you
+Pokemon
+gotta catch em all
+Gotta catch em all
+
+Yeah
+
+Every challenge along the way
+With courage I will face
+I will battle every day
+To claim my rightful place
+
+Come with me, the time is right
+Theres no better team
+Arm in arm well win the fight
+Its always been our dream
+
+Pokemon, gotta catch em all
+Its you and me
+I know its my destiny
+Pokemon
+Oh, youre my best friend
+In a world we must defend
+Pokemon, gotta catch em all
+A heart so true
+Our courage will pull us through
+
+You teach me and Ill teach you
+Pokemon
+gotta catch em all
+Gotta catch em all
+Gotta catch em all
+
+Gotta catch em all
+Gotta catch em all
+Yeah!
+
+
+Pokemon, gotta catch em all
+Its you and me
+I know its my destiny
+Pokemon
+Oh, youre my best friend
+In a world we must defend
+Pokemon, gotta catch em all
+A heart so true
+Our courage will pull us through
+
+You teach me and Ill teach you
+Pokemon
+gotta catch em all
+Gotta catch em all
+Pokemon""")   
+
+def print_cool_text():
+    print("Maybe later")
+    # print("I'll implement the rest of this script in the level-8-helper branch.")
+    # print("The rest of this script by now was already implemented. Try running git merge level-8-helper/level-8")
+
+def main():
+    print_pokemon_theme()
+    print_cool_text()
+
+if __name__ == "__main__":
+    main()
