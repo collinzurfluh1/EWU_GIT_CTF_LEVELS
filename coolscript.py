edc72350786fa4ef0b06e026a36c4c20e819dfff
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-08 13:05:01 -0800

    Update coolscript.py

--- coolscript.py
+++ coolscript.py
@@ -78,7 +78,8 @@ Pokemon""")
 def print_cool_text():
     print("Maybe later")
     # print("I'll implement the rest of this script in the level-8-helper branch.")
-    # print("The rest of this script by now was already implemented. Try running git merge level-8-helper/level-8")
+    # print("The rest of this script by now was already implemented. Try running git merge origin/mergehelper1")
+    # print("If that succeeds maybe try git push")
 
 def main():
     print_pokemon_theme()
