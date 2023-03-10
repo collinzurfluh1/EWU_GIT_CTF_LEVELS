b956d497819a3652a379bf8bb171a587dad296eb
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-10 14:34:03 -0800

    Update coolscript.py

--- coolscript.py
+++ coolscript.py
@@ -6,8 +6,10 @@ import base64
 def print_all_resources():
     for resource_path in glob.glob("./resources/*"):
         with open(resource_path, "r") as resource_file:
+            print("\n=== Printing resource " + resource_path + " ===\n")
             resource = resource_file.read()
             decoded_resource = base64.b64decode(resource).decode()
+            print(decoded_resource)
 
 def main():
     print_all_resources()
