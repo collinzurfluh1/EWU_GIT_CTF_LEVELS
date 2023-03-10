48dda152fedc0da0f38c9ec39c172a5f7f5c541d
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-10 13:23:36 -0800

    Update coolscript.py

--- coolscript.py
+++ coolscript.py
@@ -1,95 +1,16 @@
 #!/usr/bin/env python3
 
-def print_pokemon_theme():
-    print("""I wanna be the very best
-Like no one ever was 
-To catch them is my real test 
-To train them is my cause
+import glob
+import base64
 
-I will travel across the land
-Searching far and wide
-Each Pokemon to understand
-The power thats inside
-
-Pokemon, gotta catch em all
-Its you and me
-I know its my destiny
-Pokemon
-Oh, youre my best friend
-In a world we must defend
-Pokemon, gotta catch em all
-A heart so true
-Our courage will pull us through
-
-You teach me and Ill teach you
-Pokemon
-gotta catch em all
-Gotta catch em all
-
-Yeah
-
-Every challenge along the way
-With courage I will face
-I will battle every day
-To claim my rightful place
-
-Come with me, the time is right
-Theres no better team
-Arm in arm well win the fight
-Its always been our dream
-
-Pokemon, gotta catch em all
-Its you and me
-I know its my destiny
-Pokemon
-Oh, youre my best friend
-In a world we must defend
-Pokemon, gotta catch em all
-A heart so true
-Our courage will pull us through
-
-You teach me and Ill teach you
-Pokemon
-gotta catch em all
-Gotta catch em all
-Gotta catch em all
-
-Gotta catch em all
-Gotta catch em all
-Yeah!
-
-
-Pokemon, gotta catch em all
-Its you and me
-I know its my destiny
-Pokemon
-Oh, youre my best friend
-In a world we must defend
-Pokemon, gotta catch em all
-A heart so true
-Our courage will pull us through
-
-You teach me and Ill teach you
-Pokemon
-gotta catch em all
-Gotta catch em all
-Pokemon""")   
-    
-def best_text():
-    message = "This is even best text that you can find"
-    print(message)
-
-def print_cool_text():
-    message = "The coolest text you can find."
-    print(message)
-
-def cooler_text():
-    message = "This is even cooler text that you can find just waiting around"
-    print(message)
+def print_all_resources():
+    for resource_path in glob.glob("./resources/*"):
+        with open(resource_path, "r") as resource_file:
+            resource = resource_file.read()
+            decoded_resource = base64.b64decode(resource).decode()
 
 def main():
-    print_pokemon_theme()
-    print_cool_text()
+    print_all_resources()
 
 if __name__ == "__main__":
     main()
