7b1fdf15edbeb7ae9b09525425cd9902570660c6
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-13 20:46:55 -0700

    Update coolscript.py

--- coolscript.py
+++ coolscript.py
@@ -3,7 +3,6 @@
 def print_something_cool():
     # I'll finish the rest of the work in another branch... zZzZ
     raise NotImplementedError("I'll implement the rest of this script in the level-8. Use git merge origin/mergehelper1")
-    
 
 def main():
     print_something_cool()
