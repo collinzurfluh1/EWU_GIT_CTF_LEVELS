4a83faf1abc51c75571998acda2eb84507821f73
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-13 20:03:08 -0700

    Update and rename coolscript.py to mergehelper4.py

---
+++ mergehelper4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import base64
+
+def print_something_cool():
+    something_cool = 'to merge or not to merge... hmm'
+    print(something_cool)
+    
+def main():
+    print_something_cool()
+
+if __name__ == "__main__":
+    main()
