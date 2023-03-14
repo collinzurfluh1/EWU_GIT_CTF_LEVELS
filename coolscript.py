4b44b698cc24f24708eb30b8d6134bb3c4198983
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-14 08:40:56 -0700

    Update coolscript.py

--- coolscript.py
+++ coolscript.py
@@ -1,88 +1,10 @@
 #!/usr/bin/env python3
 
-def print_pokemon_theme():
-    print("""I wanna be the very best
-Like no one ever was 
-To catch them is my real test 
-To train them is my cause
-
-I will travel across the land
-Searching far and wide
-Each Pokemon to understand
-The power thats inside
-
-Pokemon, gotta catch em all
-Its you and me
-I know its my destiny
-Pokemon
-Oh, youre my best friend
-In a world we must defend
-Pokemon, gotta catch em all
-A heart so true
-Our courage will pull us through
-
-You teach me and Ill teach you
-Pokemon
-gotta catch em all
-Gotta catch em all
-
-Yeah
-
-Every challenge along the way
-With courage I will face
-I will battle every day
-To claim my rightful place
-
-Come with me, the time is right
-Theres no better team
-Arm in arm well win the fight
-Its always been our dream
-
-Pokemon, gotta catch em all
-Its you and me
-I know its my destiny
-Pokemon
-Oh, youre my best friend
-In a world we must defend
-Pokemon, gotta catch em all
-A heart so true
-Our courage will pull us through
-
-You teach me and Ill teach you
-Pokemon
-gotta catch em all
-Gotta catch em all
-Gotta catch em all
-
-Gotta catch em all
-Gotta catch em all
-Yeah!
-
-
-Pokemon, gotta catch em all
-Its you and me
-I know its my destiny
-Pokemon
-Oh, youre my best friend
-In a world we must defend
-Pokemon, gotta catch em all
-A heart so true
-Our courage will pull us through
-
-You teach me and Ill teach you
-Pokemon
-gotta catch em all
-Gotta catch em all
-Pokemon""")   
-
-def print_cool_text():
-    message = "The coolist text you can finder.   "
-    print(message)
-
+def print_good_text():
+    print("Never gonna giv u up neva gonna let u dowwwnnnn never gonna run around and desert you")   
 
 def main():
-    print_pokemon_theme()
-    print_cool_text()
+    print_good_text()
 
 if __name__ == "__main__":
     main()
