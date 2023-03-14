bfc140db0c973f1cfbb7d532e8717871d3b91ba9
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-14 08:49:38 -0700

    Create coolestscript.py

---
+++ coolestscript.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def count_to_10_really_bad():
+    print("1")   
+    print("2")   
+    print("3")   
+    print("4")   
+    print("5")   
+    print("6")   
+    print("7")   
+    print("8")   
+    print("9")   
+    print("10")   
+
+def main():
+    count_to_10_really_bad()
+
+if __name__ == "__main__":
+    main()
