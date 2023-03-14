13fa3c54c041b2c266c4865fb32cbec015a20be1
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-13 19:59:41 -0700

    Update and rename coolscript.py to mergerscript.py

---
+++ mergerscript.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def print_pokemon_theme():
+    print("""
+def print_cool_text():
+    print("Maybe later")
+    # print("I'll implement the rest of this script in the level-8-helper branch.")
+    # print("The rest of this script by now was already implemented. Try running git merge origin/mergehelper1")
+    # print("If that succeeds maybe try git push")
+
+def main():
+    print_pokemon_theme()
+    print_cool_text()
+
+if __name__ == "__main__":
+    main()
