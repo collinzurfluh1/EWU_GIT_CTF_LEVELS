d6d40a8002e321d91e5c0f4a9a462f679a504cb0
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-13 20:23:38 -0700

    Update mergerscript.py

--- mergerscript.py
+++ mergerscript.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 
-
 def print_cool_text():
     print("Maybe later")
     # print("I'll implement the rest of this script in the mergehelper4 branch.")
     # print("The rest of this script by now was already implemented. Try running git merge origin/mergehelper4")
     # print("If that succeeds maybe try git push")
-
+    
 def main():
-    print_cool_text()
+    print_something_cool()
 
 if __name__ == "__main__":
     main()
