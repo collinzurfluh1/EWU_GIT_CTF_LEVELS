9737f255c4cd8ec42f45e78ae2b8d11bb77722b1
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-14 08:50:54 -0700

    Update coolscript.py

--- coolscript.py
+++ coolscript.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+def you_wanna_keep_this():
+    print("No really you want to keep this. There is nothing in this world more important to this text, you, and I.")
+
 def print_good_text():
     print("Never gonna give you up never gonna let u down never gonna run around and desert you")   
 
