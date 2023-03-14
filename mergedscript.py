78dfcb13b22b3fedcd4b100d1f65e1ba7bf6789f
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-13 23:26:56 -0700

    Create mergedscript.py

---
+++ mergedscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import glob
+import base64
+
+def print_merge_success():
+    print("Congrats you merged successfully")
+
+def main():
+    print_merge_success()
+
+if __name__ == "__main__":
+    main()
