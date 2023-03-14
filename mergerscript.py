37adda211cf1eb34812122f6fac63c187e39fa66
Collin Zurfluh <91215036+collinzurfluh1@users.noreply.github.com>
2023-03-13 20:01:11 -0700

    Update mergerscript.py

--- mergerscript.py
+++ mergerscript.py
@@ -3,12 +3,11 @@
 
 def print_cool_text():
     print("Maybe later")
-    # print("I'll implement the rest of this script in the level-8-helper branch.")
-    # print("The rest of this script by now was already implemented. Try running git merge origin/mergehelper1")
+    # print("I'll implement the rest of this script in the mergehelper4 branch.")
+    # print("The rest of this script by now was already implemented. Try running git merge origin/mergehelper4")
     # print("If that succeeds maybe try git push")
 
 def main():
-    print_pokemon_theme()
     print_cool_text()
 
 if __name__ == "__main__":
